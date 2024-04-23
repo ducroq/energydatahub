016df7eb1043a1cbae0eccb73832b93d714d8add
jeroen_veen <jveen1@gmail.com>
2024-04-23 11:37:51 +0200

    First version of server  created

--- main.py
+++ main.py
@@ -1,42 +1,69 @@
-https://www.geeksforgeeks.org/socket-programming-python/
+import asyncio
+import json
+import configparser
+import logging
+import datetime
+from cerberus import Validator
+
+SETTINGS_FILE_NAME = 'secrets.ini'
+LOGGING_FILE_NAME = 'server.log'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[logging.StreamHandler(), logging.FileHandler(LOGGING_FILE_NAME)]
+)
+
+config = configparser.ConfigParser()
+config.read(SETTINGS_FILE_NAME)
+server_ip = config['server']['ip']
+server_port = int(config['server']['port'])
+
+def get_timestamp():
+  """Returns the current timestamp in ISO 8601 format (YYYY-MM-DD HH:MM:SS)."""
+  now = datetime.datetime.now()
+  return now.isoformat()
+
+async def handle_client(reader, writer):
+    while True:
+        try:
+            data = await reader.read(1024)
+            if not data:
+                break
+            logging.info(f"Client request received: {data}")
+            request = data.decode().lower()
+            if request == "get_data":
+                try:
+                    price_value_1 = 255.0
+                    json_data = {
+                        "timestamp": get_timestamp(),
+                        "electricity_prices": {
+                            "source_1": price_value_1,
+                            # ...
+                            }
+                    }
+
+                    writer.write(json.dumps(json_data).encode())
+                    await writer.drain()
+                except Exception as e:
+                    logging.error(f"Error writing data to client: {e}")
+            else:
+                writer.write(b"Invalid request")
+                await writer.drain()
+        except Exception as e:
+            logging.error(f"Error handling client connection: {e}")
+    writer.close()
 
 
-# first of all import the socket library 
-import socket			 
+async def main():
+    try:
+        server = await asyncio.start_server(handle_client, server_ip, server_port)
+        async with server:
+            logging.info(f"Server {server_ip}:{server_port} started successfully")
+            await server.serve_forever()
+    except Exception as e:
+        logging.critical(f"Error starting server: {e}")
 
-# next create a socket object 
-s = socket.socket()		 
-print ("Socket successfully created")
+asyncio.run(main())
 
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345			
 
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests 
-# coming from other computers on the network 
-s.bind(('', port))		 
-print ("socket binded to %s" %(port)) 
-
-# put the socket into listening mode 
-s.listen(5)	 
-print ("socket is listening")		 
-
-# a forever loop until we interrupt it or 
-# an error occurs 
-while True: 
-
-    # Establish connection with client. 
-    c, addr = s.accept()	 
-    print ('Got connection from', addr )
-
-    # send a thank you message to the client. encoding to send byte type. 
-    c.send('Thank you for connecting'.encode()) 
-
-    # Close the connection with the client 
-    c.close()
-
-    # Breaking once connection closed
-    break
