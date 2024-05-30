f66c27a632bb78ad1c488982ef32a23841ff3ca7
jeroen_veen <jveen1@gmail.com>
2024-05-30 14:17:20 +0200

    got Marc's module working

--- testMeteoApi.py
+++ testMeteoApi.py
@@ -1,16 +1,18 @@
 """Example Python script using the Meteoserver module."""
 
 import meteoserver as meteo
+from configparser import ConfigParser 
 
-myKey = '7daf22bed0pip'    # My Meteoserver API key - put your OWN key here!
-myLocation = 'Arnhem'  # My location
+configur = ConfigParser() 
+configur.read('secrets.ini')
+my_api_key = configur.get('api_keys', 'meteo')
+plaats = configur.get('location', 'plaats')
+
 
 # Weather forecast #################################################################################
 
 # Print some help:
 meteo.print_help_weatherforecast()
-
-location = 'Arnhem'  # Ensure we always have a location 'name' to write to file.
 
 # Read weather-forecast data from file:
 # data = meteo.read_json_file_weatherforecast('WeatherForecast1.json', full=True)  # Option 1: HARMONIE/HiRLAM (48 (42?) hours)
@@ -19,16 +21,16 @@ location = 'Arnhem'  # Ensure we always have a location 'name' to write to file.
 # data, location = meteo.read_json_file_weatherforecast('WeatherForecast2.json', full=True, loc=True, numeric=False)
 
 # Get weather-forecast data from server:
-# data = meteo.read_json_url_weatherforecast(myKey, myLocation, model='HARMONIE')  # Option 1: HARMONIE/HiRLAM
+data = meteo.read_json_url_weatherforecast(my_api_key, plaats, model='HARMONIE')  # Option 1: HARMONIE/HiRLAM
 # data = meteo.read_json_url_weatherforecast(myKey, myLocation)  # Option 2 (default): GFS, useful columns only, no location
 # Option 2, with ALL columns and location; don't convert to numerical format, to allow writing to file later:
-data, location = meteo.read_json_url_weatherforecast(myKey, myLocation, full=True, loc=True, numeric=False)
+# data, location = meteo.read_json_url_weatherforecast(myKey, myLocation, full=True, loc=True, numeric=False)
 
 # Print the data:
 print(data)
 
 # Write the downloaded data to a json file:
-meteo.write_json_file_weatherforecast('WeatherForecast3.json', location, data)
+meteo.write_json_file_weatherforecast('WeatherForecast3.json', plaats, data)
 
 
 
@@ -45,7 +47,7 @@ meteo.print_help_sunData()
 # Get Meteoserver Sun data from the server for the given location (and key):
 # current, forecast = meteo.read_json_url_sunData(myKey, myLocation)
 # Return the location; don't convert to numerical format, to allow writing to file later:
-current, forecast, location = meteo.read_json_url_sunData(myKey, myLocation, loc=True, numeric=False)
+current, forecast, location = meteo.read_json_url_sunData(my_api_key, plaats, loc=True, numeric=False)
 
 # Print the current-weather and forecast dataframes:
 print("\nCurrent Sun/weather observation from a nearby station:")
