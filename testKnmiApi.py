772c5f19ec4b4931c651ce30d26d35edbb86536b
jeroen_veen <jveen1@gmail.com>
2024-05-30 13:29:34 +0200

    .

--- testKnmiApi.py
+++ testKnmiApi.py
@@ -3,7 +3,10 @@ from datetime import datetime, date, timedelta
 
 today = date.today()
 yesterday = date.today() - timedelta(days=1)
+tomorrow = date.today() + timedelta(days=1)
 
 df = knmi.get_day_data_dataframe(stations=[260], start=yesterday, end=today) 
+print(df)
 
+df = knmi.get_forecast_dataframe()
 print(df)
