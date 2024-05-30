2498ce3169153938c49d24eedd1f30568c2d4df2
jeroen_veen <jveen1@gmail.com>
2024-05-30 14:17:35 +0200

    geographical coordinates from plaatsnaam

--- testOpenWeather.py
+++ testOpenWeather.py
@@ -2,13 +2,25 @@ from configparser import ConfigParser
 import requests
 import json
 
+plaats = 'Arnhem'
+
 configur = ConfigParser() 
 configur.read('secrets.ini')
 my_api_key = configur.get('api_keys', 'openweather')
-lattitude = configur.get('location', 'lattitude')
-longitude = configur.get('location', 'longitude')
+# latitude = configur.get('location', 'latitude')
+# longitude = configur.get('location', 'longitude')
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lattitude}&lon={longitude}&appid={my_api_key}"
+url = f"http://api.openweathermap.org/geo/1.0/direct?q={plaats},?,NL&limit=1&appid={my_api_key}"
+response = requests.get(url)
+
+if response.status_code == 200:
+  data = response.json()
+  print(json.dumps(data, indent=4))
+  print(data[0]["lat"], data[0]["lon"])
+  latitude = data[0]["lat"]
+  longitude = data[0]["lon"]
+
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={my_api_key}"
 response = requests.get(url)
 
 if response.status_code == 200:
@@ -36,7 +48,7 @@ else:
   print("Error:", response.status_code)
 
 
-# url = f"https://api.openweathermap.org/energy/1.0/solar/data?lat={lattitude}&lon={longitude}&date={date}&appid={my_api_key}"
+# url = f"https://api.openweathermap.org/energy/1.0/solar/data?lat={latitude}&lon={longitude}&date={date}&appid={my_api_key}"
 url = f"https://api.openweathermap.org/energy/1.0/solar/data?lat=60.45&lon=-38.67&date=2023-03-30&tz=+03:00&appid={my_api_key}"
 response = requests.get(url)
 
