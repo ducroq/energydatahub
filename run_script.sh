97410a28d4349047d8fa2ae8120a217091a454dd
jeroen_veen <jveen1@gmail.com>
2024-06-13 19:39:25 +0200

    updata shell script

--- run_script.sh
+++ run_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Path to your virtual environment
-VENV_PATH="/home/pi/energyDataScraper"
+VENV_PATH="/home/pi/energyDataHub"
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
 # Execute your Python script
-python /home/pi/energyDataScraper/energyDataScraper.py
+python /home/pi/energyDataHub/energyDataScraper.py
 
 # Deactivate the virtual environment
 deactivate
