70009d25795b0c6b9289a8d43b53c8045b8c7fe2
jeroen_veen <jveen1@gmail.com>
2024-09-15 14:08:40 +0200

    luchtmeet fetching implemented

---
+++ helpers.py
@@ -0,0 +1,34 @@
+from math import cos, asin, sqrt
+
+def convert_value(value):
+    if type(value) == int or type(value) == float:
+        return value
+    if value == '-':
+        return None  # Using None instead of NaN for JSON compatibility
+    elif value.lower() == 'none':
+        return None
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value  # Keep as string if it's not a number
+        
+def distance(lat1, lon1, lat2, lon2):
+    p = 0.017453292519943295
+    a = (
+        0.5
+        - cos((lat2 - lat1) * p) / 2
+        + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    )
+    distance = 12742 * asin(sqrt(a))
+    return distance
+
+def closest(data, v):
+    return min(
+        data,
+        key=lambda p: distance(
+            v["latitude"], v["longitude"], p["latitude"], p["longitude"]
+        ),
+    )        
