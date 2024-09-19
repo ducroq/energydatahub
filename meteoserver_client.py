ccf9397530d6891d07994a0d0228667a662a3c85
jeroen_veen <jveen1@gmail.com>
2024-09-19 17:27:35 +0200

    response

--- meteoserver_client.py
+++ meteoserver_client.py
@@ -64,7 +64,7 @@ async def get_MeteoServer_sun_forecast(api_key: str, latitude: float, longitude:
                     return processed_data
 
                 response_data = await response.json()
-                # print(response_data)
+                print(response_data)
                 processed_data['metadata'] = {
                     "plaats": response_data['plaatsnaam'][0]['plaats'],
                     # "station": response_data['current'][0]['station'], 
